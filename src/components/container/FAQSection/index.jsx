9bc98d17fded1e811390c94b75591f1fdcc7e4b9
santani423 <santani423@gmail.com>
2025-12-15 00:40:52 +0700

    oke

---
+++ src/components/container/FAQSection/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FAQ() {
+  return (
+    <section className="min-h-[60vh] flex items-center justify-center bg-[#18111A] text-white">
+      <h2 className="text-3xl font-bold">FAQ Page</h2>
+    </section>
+  );
+}
